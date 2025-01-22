8c0f2ebe3cb31d34042b83c14862198eb5cd3450
bc299 <bc299@duke.edu>
2025-01-22 14:19:09 -0500

    get ollama working with model client

--- model_client.py
+++ model_client.py
@@ -3,4 +3,8 @@ from langchain_ollama import OllamaLLM
 class ModelClient:
     def __init__(self):
         # Initialize Anthropic (Claude)
-        self.ollama = 
+        self.ollama = OllamaLLM(model="llama3.2")
+
+    def ask_llama(self, prompt):
+        response = self.ollama.invoke(prompt)
+        return response
